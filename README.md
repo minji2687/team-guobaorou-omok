b552b9230762584cbe406e06ea9d0511e1d1cf04
Jo Minji <mingi2687@gamil.com>
2018-10-26 17:59:00 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# FDS 해커톤 템플릿 프로젝트
+
+오목(omok)
+
+# Game Rules
+
+- 초록돌 : player1 , 파란돌 : player2
+- 사용자가 번갈아 가면서 돌을 둔다.
+- 가로 방향, 세로 방향 혹은 대각선 방향으로 5개 돌이 연속으로 먼저 놓여지는 사람이 승리.
+- 돌이 있는 자리에는 돌을 둘 수 없다.
+
+
+# Game function
+
+- 시작 시 사용자가 원하는 닉네임 입력 가능. 입력하지 않을 시, 자동적으로 Player1 혹은 Player2로 닉네임 지정.
+- 현재 차례의 사용자 닉네임 색깔 표시.
+- 게임 중 reset 버튼 클릭 시 해당 바둑판만 초기화.
+- 게임 종료 시 창의 restart 버튼 클릭 시 게임 완전 초기화.
+
+# Team Member
+
+- 김현준 [@joonkim0625](https://github.com/joonkim0625)
+- 조민지 [@minji2687](https://github.com/minji2687)
+- 안세나 [@serendipity](https://github.com/sena-a)
---
+++ src/index.js
@@ -0,0 +1,325 @@
+// 공통 변수
+const startPage = document.querySelector('.start-page')
+
+const winnerPage = document.querySelector('.winner-page')
+
+const startPlayersEl = document.querySelector('.start-players')
+
+const player1El = document.querySelector('.player1-name')
+
+const player2El = document.querySelector('.player2-name')
+
+
+// 사용자 이름 입력 값 함수
+function player1(name){
+  if(name === ''){return 'Player1';}else{
+    return name;
+  }
+
+}
+
+function player2(name){
+  if(name === ''){return 'Player2';}else{
+    return name;
+  }
+}
+
+
+// 게임 로직
+
+// 현재 바둑돌 색깔 - 흑돌일 시 1, 백돌일 시 2
+
+let currentStone = 1
+
+// 게임판 초기값
+let boardState = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+// 게임 상태
+function drawBoard() {
+  document.querySelectorAll(".row").forEach((rowEl, rowIndex) => {
+    rowEl.querySelectorAll(".col").forEach((colEl, colIndex) => {
+      if (boardState[rowIndex][colIndex] === 1) {
+        colEl.classList.add("black");
+      } else {
+        colEl.classList.remove("black");
+      }
+      if (boardState[rowIndex][colIndex] === 2) {
+        colEl.classList.add("white");
+      } else {
+        colEl.classList.remove("white");
+      }
+    });
+  });
+
+// 승리 시 화면 변화
+  if (omok(boardState) === 1) {
+    document.querySelector('.winner').textContent = player1(startPlayersEl.player1.value)
+    document.querySelector('.winner-page').classList.add('winner1-act')
+  } else if(omok(boardState) === 2) {
+    document.querySelector('.winner').textContent = player2(startPlayersEl.player2.value)
+    document.querySelector('.winner-page').classList.add('winner2-act')
+  }
+}
+
+
+// 승리 체크 로직
+
+function omok(arr) {
+
+  // 가로줄
+  for (let i = 0; i < 15; i++) {
+    let currentPlayer;
+    let count;
+    for (let j = 0; j < 15; j++) {
+      if (currentPlayer !== arr[i][j]) {
+        currentPlayer = arr[i][j];
+        count = 1;
+      } else {
+        count++;
+      }
+      if ((currentPlayer === 1 || currentPlayer === 2) && count === 5) {
+        return currentPlayer;
+      }
+    }
+  }
+
+  // 세로줄
+  for (let i = 0; i < 15; i++) {
+    let currentPlayer;
+    let count;
+    for (let j = 0; j < 15; j++) {
+      if (currentPlayer !== arr[j][i]) {
+        currentPlayer = arr[j][i];
+        count = 1;
+      } else {
+        count++;
+      }
+      if ((currentPlayer === 1 || currentPlayer === 2) && count === 5) {
+        return currentPlayer;
+      }
+    }
+  }
+
+  // 대각선 왼쪽 : 중앙을 가로지르는 줄
+  {
+    let currentPlayer;
+    let count;
+    for (let i = 0; i < 15; i++) {
+      if (currentPlayer !== arr[i][i]) {
+        currentPlayer = arr[i][i];
+        count = 1;
+      } else {
+        count++;
+      }
+      if ((currentPlayer === 1 || currentPlayer === 2) && count === 5) {
+        return currentPlayer;
+      }
+    }
+  }
+  // 대각선 왼쪽 : 중앙을 중심으로 좌측 줄들 - 5개가 연속으로 놓일 수 있는 줄까지만 체크
+  {
+    let currentPlayer;
+    let count;
+    for (let i = 1; i < 12; i++) {
+      for (let j = 0; j < 15 - i; j++) {
+        if (currentPlayer !== arr[i + j][j]) {
+          currentPlayer = arr[i + j][j];
+          count = 1;
+        } else {
+          count++;
+        }
+        if ((currentPlayer === 1 || currentPlayer === 2) && count === 5) {
+          return currentPlayer;
+        }
+      }
+    }
+  }
+  // 대각선 왼쪽 : 중앙을 중심으로 우측 줄들 - 5개가 연속으로 놓일 수 있는 줄까지만 체크
+  {
+    let currentPlayer;
+    let count;
+    for (let i = 1; i < 12; i++) {
+      for (let j = 0; j < 15 - i; j++) {
+        if (currentPlayer !== arr[j][i + j]) {
+          currentPlayer = arr[j][i + j];
+          count = 1;
+        } else {
+          count++;
+        }
+        if ((currentPlayer === 1 || currentPlayer === 2) && count === 5) {
+          return currentPlayer;
+        }
+      }
+    }
+  }
+  // 대각선 오른쪽 : 중앙을 가로지르는 줄
+  {
+    let currentPlayer;
+    let count;
+    for (let i = 0; i < 15; i++) {
+      if (currentPlayer !== arr[i][14 - i]) {
+        currentPlayer = arr[i][14 - i];
+        count = 1;
+      } else {
+        count++;
+      }
+      if ((currentPlayer === 1 || currentPlayer === 2) && count === 5) {
+        return currentPlayer;
+      }
+    }
+  }
+  // 대각선 오른쪽 : 중앙을 중심으로 좌측 줄들 - 5개가 연속으로 놓일 수 있는 줄까지만 체크
+  {
+    let currentPlayer;
+    let count;
+    for (let i = 1; i < 12; i++) {
+      for (let j = 0; j < 15 - i; j++) {
+        if (currentPlayer !== arr[j][14 - j - i]) {
+          currentPlayer = arr[j][14 - j - i];
+          count = 1;
+        } else {
+          count++;
+        }
+        if ((currentPlayer === 1 || currentPlayer === 2) && count === 5) {
+          return currentPlayer;
+        }
+      }
+    }
+  }
+  // 대각선 오른쪽: 중앙을 중심으로 우측 줄들 - 5개가 연속으로 놓일 수 있는 줄까지만 체크
+  {
+    let currentPlayer;
+    let count;
+    for (let i = 1; i < 12; i++) {
+      for (let j = 0; j < 15 - i; j++) {
+        if (currentPlayer !== arr[j + i][14 - j]) {
+          currentPlayer = arr[j + i][14 - j];
+          count = 1;
+        } else {
+          count++;
+        }
+        if ((currentPlayer === 1 || currentPlayer === 2) && count === 5) {
+          return currentPlayer;
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+// 게임 진행 로직
+document.querySelectorAll(".row").forEach((rowEl, rowIndex) => {
+  rowEl.querySelectorAll(".col").forEach((colEl, colIndex) => {
+    colEl.addEventListener("click", e => {
+        if (!omok(boardState)) {
+          // 놓여진 돌이 없을 때만 실행
+          if (boardState[rowIndex][colIndex] === 0){
+            boardState[rowIndex][colIndex] = currentStone;
+            currentStone = boardState[rowIndex][colIndex] === 1 ? 2 : 1;
+          }
+
+          // 현재 턴 표시
+          if (currentStone === 1){
+            player1El.classList.add('current-turn')
+            player2El.classList.remove('current-turn')
+          } else if (currentStone === 2){
+            player2El.classList.add('current-turn')
+            player1El.classList.remove('current-turn')
+          }
+
+          drawBoard();
+        }
+    });
+  });
+});
+
+// 시작하기 버튼 이벤트 리스너
+document.querySelector('.start-btn').addEventListener('click', e=> {
+  startPage.classList.add('start-act')
+  player1El.classList.add('current-turn')
+  player1El.textContent = player1(startPlayersEl.player1.value)
+  player2El.textContent = player2(startPlayersEl.player2.value)
+})
+
+// 게임 종료 시 재시작 버튼 이벤트리스너
+document.querySelector('.restart-btn').addEventListener('click', e=> {
+
+  boardState = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ];
+
+  currentStone = 1;
+
+  drawBoard();
+
+  winnerPage.classList.remove('winner1-act')
+  winnerPage.classList.remove('winner2-act')
+
+  startPlayersEl.player1.value = ''
+  startPlayersEl.player2.value = ''
+
+  startPage.classList.remove('start-act');
+})
+
+
+// 리셋 버튼 이벤트리스너
+
+document.querySelector(".reset-btn").addEventListener("click", e => {
+
+  boardState = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ];
+  currentStone = 1;
+  drawBoard();
+
+  startPage.classList.add('start-act');
+});
+
+// 기본 화면
+drawBoard();
+
+
